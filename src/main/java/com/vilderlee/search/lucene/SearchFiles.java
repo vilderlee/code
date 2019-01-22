70ad1ab9de2f4460665cd3da6b255471a7c62b79
vilderlee <1010434086@qq.com>
2019-01-22 18:25:42 +0800

    Lucene代码更新

--- src/main/java/com/vilderlee/search/lucene/SearchFiles.java
+++ src/main/java/com/vilderlee/search/lucene/SearchFiles.java
@@ -15,6 +15,8 @@ import org.apache.lucene.store.FSDirectory;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <pre>
@@ -26,7 +28,7 @@ import java.nio.file.Paths;
  */
 public class SearchFiles {
 
-    public void searchFile(String indexPath,String field) {
+    public List searchFile(String indexPath, String field) {
         try {
             //指定索引库的存放地址
             Directory directory = FSDirectory.open(Paths.get(indexPath));
@@ -42,30 +44,46 @@ public class SearchFiles {
             //第一个参数是查询对象，第二个参数是查询结果返回的最大值
             TopDocs topDocs = indexSearcher.search(query, 10);
             int i = 0;
+            List list = new ArrayList<FileText>();
+
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
                 //scoreDoc.doc属性就是document对象的id
                 //int doc = scoreDoc.doc;
                 //根据document的id找到document对象
                 Document document = indexSearcher.doc(scoreDoc.doc);
                 //文件名称
-                System.out.println(document.get("fileName"));
+                String fileName = document.get("fileName");
+                System.out.println(fileName);
                 //文件大小
-                System.out.println(document.get("fileSize"));
+                String fileSize = document.get("fileSize");
+                System.out.println(fileSize);
                 //文件路径
-                System.out.println(document.get("filePath"));
+                String filePath = document.get("filePath");
+                System.out.println(filePath);
                 System.out.println("----------------------------------");
+
+                if (fileName.indexOf(field) != -1) {
+                    list.add(new FileText(fileName,fileSize,filePath));
+                }
                 System.out.println(++i);
             }
 
             indexReader.close();
+            return list;
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return null;
     }
 
     public static void main(String[] args) {
+
         SearchFiles searchFiles = new SearchFiles();
-        searchFiles.searchFile("E:\\index\\", "fileName");
+        List list = searchFiles.searchFile("E:\\index\\", "Redis");
+        list.forEach(o->{
+            System.out.println(o.toString());
+        });
     }
 }
